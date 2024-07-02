99d63f9d21918992583f188941c5cc73e2b91cf8
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-07-02 14:17:09 +0300

    all data

---
+++ src/assets/data.js
@@ -0,0 +1,103 @@
+import big_img1 from "../assets/images/big_img1.png";
+import big_img2 from "../assets/images/big_img2.png";
+import big_img3 from "../assets/images/big_img3.png";
+
+export const big_imgs = [big_img1, big_img2, big_img3];
+
+
+export const items = [
+    {
+        id: 1,
+        img: '/src/assets/images/n1.png',
+        content: 'تطريز',
+    },
+    {
+        id: 2,
+        img: '/src/assets/images/n3.jpg',
+        content: 'تطريز',
+    },
+    {
+        id: 3,
+        img: '/src/assets/images/big_img1.png',
+        content: 'تطريز',
+    },
+    {
+        id: 4,
+        img: '/src/assets/images/n5.png',
+        content: 'تطريز',
+    },
+    {
+        id: 5,
+        img: '/src/assets/images/n6.png',
+        content: 'تطريز',
+    },
+    {
+        id: 6,
+        img: '/src/assets/images/n11.png',
+        content: 'تطريز',
+    },
+    {
+        id: 7,
+        img: '/src/assets/images/n14.png',
+        content: 'تطريز',
+    },
+    {
+        id: 8,
+        img: '/src/assets/images/n18.jpg',
+        content: 'تطريز',
+    },
+    {
+        id: 9,
+        img: '/src/assets/images/n19.png',
+        content: 'تطريز',
+    },
+    {
+        id: 10,
+        img: '/src/assets/images/n22.png',
+        content: 'تطريز',
+    },
+    {
+        id: 11,
+        img: '/src/assets/images/n23.png',
+        content: 'تطريز',
+    },
+    {
+        id: 12,
+        img: '/src/assets/images/n25.png',
+        content: 'تطريز',
+    },
+];
+
+
+export const reviews = [
+    {
+        name: 'Mohamed Qadry',
+        imgURL:'src/assets/images/vector_06.jpg',
+        comment: '  تسلم إيدك ياحبيبي شغل تحفة اللهم بارك😍️😍️❤️ ',
+        rating: '4.8',
+        time:'2:53PM',
+        date:'22-1-2020', 
+    },
+    {
+        name: 'Mohamed Qadry',
+        imgURL:'src/assets/images/vector_06.jpg',
+        comment: '  تسلم إيدك ياحبيبي شغل تحفة اللهم بارك😍️😍️❤️ ',
+        rating: '4.8',
+        time:'2:53PM',
+        date:'22-1-2020', 
+    },
+    
+];
+
+import { BsFacebook , BsInstagram } from "react-icons/bs";
+
+export const contactInfo = [
+    {
+        name: "Facebook",
+        icon: `${BsFacebook }`,
+    },
+    {
+        name: "Facebook",
+        icon: `${BsFacebook}`,
+    },
+];
